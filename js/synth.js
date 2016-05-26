9ce7549000b92f19421950017af037cd8a52feba
mradambeck <mr.adambeck@gmail.com>
2016-05-26 14:01:57 -0700

    fixes biquad connections

--- js/synth.js
+++ js/synth.js
@@ -1,47 +1,52 @@
+// Pull AudioContext
 var context = new AudioContext();
-var speaker = context.destination;
+var speaker = context.destination; // OUTPUT
 const NOW = context.currentTime;
 
 class Synth {
-  constructor (waveform = 'triangle', connection = speaker) {
+  constructor (waveform, connection) {
     let volume = this.volume = context.createGain();
     volume.gain.value = 0.15;
 
-    // Play a note
+    // To play a note
     this.playNote = function (frequency = 440, startTime = NOW, duration = 0.5, wave = waveform) {
+
       // Setup Oscillator
       let osc = this.osc = context.createOscillator();
       osc.type = wave;
       osc.frequency.value = frequency;
 
-      //// Subtle fade out of note
-      // volume.gain.setValueAtTime(0.2, startTime + duration - 0.05);
-      // volume.gain.linearRampToValueAtTime(0, startTime + duration);
-
+      // Set length of note
       let stopTime = startTime + duration;
 
       // Start and stop note
       osc.start(startTime);
       osc.stop(stopTime);
-      osc.connect(volume);
+      osc.connect(volume); // Connect Oscillator to Volume
     };
 
-    volume.connect(connection);
+    volume.connect(connection); // Connect Synth to next path (Filter)
   }
 }
 
 class FilterBank {
   constructor (filterValue, detune, q, connection) {
-    this.filter = function (filterValue = 15000, detune = 1000, q = 20, connection = speaker) {
-      let biquadFilter = this.biquadFilter = context.createBiquadFilter();
-      biquadFilter.frequency.value = filterValue;
-      biquadFilter.detune.value = detune;
-      biquadFilter.Q.value = q;
-      biquadFilter.connect(connection);
-    };
-    let input = this.filter.biquadFilter;
+
+    // Biquad Filter
+      let bq = this.biquadFilter = context.createBiquadFilter();
+      bq.frequency.value = filterValue;
+      bq.detune.value = detune;
+      bq.Q.value = q;
+      bq.connect(connection);
+
   }
 }
+
+// Create output
+var amplifier = new Object();
+  amplifier.input = context.createGain();
+  amplifier.input.gain.value = 1;
+  amplifier.input.connect(speaker);
 
 // Note frequencies
 var   a1 = 55, bb1 = 58.27, b1 = 61.74, c2 = 65.41, db2 = 69.30, d2 = 73.42, eb2 = 77.78,
@@ -51,6 +56,6 @@ var   a1 = 55, bb1 = 58.27, b1 = 61.74, c2 = 65.41, db2 = 69.30, d2 = 73.42, eb2
       db4 = 277.18, d4 = 293.66, eb4 = 311.13, e4 = 329.63, f4 = 349.23, gb4 = 369.99
 ;
 
-var bq = new FilterBank();
-var osc1 = new Synth('triangle', bq.input);
+var fx = new FilterBank(3000, 1000, 20, amplifier.input);
+var osc1 = new Synth('triangle', fx.biquadFilter);
 osc1.playNote();
