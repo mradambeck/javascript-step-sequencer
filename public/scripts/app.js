67170b4046a836c7d75cb2300fd075b3b587e5fd
mradambeck <mr.adambeck@gmail.com>
2016-05-31 14:57:49 -0700

    grid generated via angular

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,7 @@
+angular
+  .module('jsSynth', [])
+  .controller('GridController', GridController);
+
 class Synth {
   constructor (connection, the_context) {
     this.now = the_context.currentTime;
@@ -170,7 +174,28 @@ function Note() {
   this.x = this.makePitch(this.pitches.x);
 }
 
+////////////////////////
+// Angular controller //
+////////////////////////
 
+function GridController () {
+  var vm = this;
+  let note = new Note();
+  pitchArray = ['b','bb','a','ab','g','gb','f','e','eb','d','db','c'];
+  vm.gridL = [
+    { name: "0", pitches: pitchArray },
+    { name: "1", pitches: pitchArray },
+    { name: "2", pitches: pitchArray },
+    { name: "3", pitches: pitchArray }
+  ];
+
+  vm.gridR = [
+    { name: "4", pitches: pitchArray },
+    { name: "5", pitches: pitchArray },
+    { name: "6", pitches: pitchArray },
+    { name: "7", pitches: pitchArray }
+  ];
+}
 
 ////////////
 // jQuery //
