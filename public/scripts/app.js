427cd29215f3f0826fe708b4ecbc66e4c8c57b5e
mradambeck <mr.adambeck@gmail.com>
2016-06-02 17:46:54 -0700

    octave buttons working, though not super happy about the lack of DRY code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,7 +17,7 @@ $(function(){
   var note = new Note(); // allows you to generate octaves of notes dynamically
   var pattern = [ note.x(0), note.x(0), note.x(0), note.x(0), note.x(0), note.x(0), note.x(0), note.x(0) ]; // notes in the measure
   var patternString = [ 'x(0)', 'x(0)', 'x(0)', 'x(0)', 'x(0)', 'x(0)', 'x(0)', 'x(0)' ]; // string version (used in recall of pattern)
-
+  var bpm = $('#bpm').val(); // Set beats per minute (calculated to milliseconds within Loop object)
   var userId = '';
 
   if (window.location.pathname === "/"){
@@ -39,8 +39,8 @@ $(function(){
       error: handleUserError
     });
 
-
   }
+
 
   // adds styling to grid to show what notes are active
   var activateGrid = function (arr){
@@ -71,15 +71,12 @@ $(function(){
   }
 
   function handleNoUserSuccess(json) {
-
   }
 
   function handleNoUserError(xhr, status, errorThrown) {
     console.log(xhr, status, errorThrown);
   }
 
-
-  var bpm = $('#bpm').val(); // Set beats per minute (calculated to milliseconds within Loop object)
 
   // Selecting notes
   $(".note").click(function(){
@@ -101,11 +98,28 @@ $(function(){
     console.log(patternString);
   });
 
+  var grabNoteOnOctaveClick = function(){
+    let beatData = $(this).attr('data-column');
+    let activeNote = $(`button[data-column="${beatData}"].active`).attr('data-note');
+    let noteData = 'note.' + activeNote;
+    let octId = `o${beatData}`;
+    let octCount = parseInt($(`#${octId}`).text());
+    let stringOfNoteFunction = `${activeNote}(${octCount})`;
+    pattern[beatData] = eval(noteData)(octCount);
+    patternString[beatData] = stringOfNoteFunction;
+    // return octCount;
+  };
+
   // Changing Octaves
   $("button.octave-up").click(function(){
     let beatData = $(this).attr('data-column');
+    let activeNote = $(`button[data-column="${beatData}"].active`).attr('data-note');
+    let noteData = 'note.' + activeNote;
     let octId = `o${beatData}`;
     let octCount = parseInt($(`#${octId}`).text());
+    let stringOfNoteFunction = `${activeNote}(${octCount})`;
+    pattern[beatData] = eval(noteData)(octCount+1);
+    patternString[beatData] = stringOfNoteFunction;
     if (octCount < 8 ){
       let newCount = (octCount + 1);
       $(`#${octId}`).text(newCount);
@@ -113,12 +127,18 @@ $(function(){
   });
   $("button.octave-down").click(function(){
     let beatData = $(this).attr('data-column');
+    let activeNote = $(`button[data-column="${beatData}"].active`).attr('data-note');
+    let noteData = 'note.' + activeNote;
     let octId = `o${beatData}`;
     let octCount = parseInt($(`#${octId}`).text());
+    let stringOfNoteFunction = `${activeNote}(${octCount})`;
+    pattern[beatData] = eval(noteData)(octCount-1);
+    patternString[beatData] = stringOfNoteFunction;
     if (octCount > 0){
       let newCount = (octCount - 1);
       $(`#${octId}`).text(newCount);
     }
+
   });
 
   // Starting the Loop
